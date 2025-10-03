5d6995c7f3564cadbbb394b3cac5c9cd6fe98bdb
GoeranBecker <goeran.sven.becker@gmail.com>
2025-10-03 12:17:02 +0200

    working raw websockets, active client, echo server

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, redirect, url_for
+import websockets as ws
+import asyncio
+from threading import Thread
 
 
 app = Flask(__name__, template_folder="../../templates", static_folder="../../static")
@@ -9,5 +12,28 @@ def home():
     return render_template("base.html")
 
 
+
+async def handler(websocket):
+    await websocket.send("Hello from asyncio server!")
+
+    async for message in websocket:
+        print(f"Received: {message}")
+        await websocket.send(f"Echo: {message}")
+
+
+async def main():
+    async with ws.serve(handler, "localhost", 8765):
+            print("Asyncio WebSocket server running on ws://localhost:8765")
+            await asyncio.Future()
+
+def startWs():
+    asyncio.run(main())
+
+def startFlask():
+    app.run(debug = True, use_reloader=False)
+
+
 if __name__ == "__main__":
+    ws_thread = Thread(target=startWs, daemon=True)
+    ws_thread.start()
     app.run(debug = True)
--- src/frontend/index.ts
+++ src/frontend/index.ts
@@ -1,1 +1,26 @@
-console.log("hello world");
+const socket = new WebSocket("ws://localhost:8765");
+
+socket.onopen = () => {
+    console.log("Connected to server");
+    socket.send("Hello from the browser!");
+};
+
+socket.onmessage = (event) => {
+    console.log("Message from server:", event.data);
+};
+
+socket.onclose = () => {
+    console.log("Disconnected from server");
+};
+
+socket.onerror = (err) => {
+    console.error("Socket error:", err);
+};
+
+const button = document.getElementById("sendButton");
+let counter = 1;
+
+button.addEventListener("click", () => {
+    socket.send(`Message ${counter}`);
+    counter++;
+})
